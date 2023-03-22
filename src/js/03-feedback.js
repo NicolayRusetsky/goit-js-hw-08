8f5f6aed64377e8d079425aaaeeed7e9d9c68278
NicolayRusetsky <96424328+NicolayRusetsky@users.noreply.github.com>
2023-03-22 20:38:30 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,7 @@ import { throttle } from 'lodash';
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
-const LOCALSTORAGE_KEY = 'feedback-form-state'; // Ключ для сховища
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener(
   'input',
